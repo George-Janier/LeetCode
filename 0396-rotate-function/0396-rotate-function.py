0905e2ccbb3bd9844859e25c50b86df519fbed40
George Janier <georgejanier01@gmail.com>
2026-05-06 13:42:40 +0530

    Time: 155 ms (81.15%), Space: 19 MB (95.98%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.py
+++ 0396-rotate-function/0396-rotate-function.py
@@ -4,10 +4,10 @@ class Solution(object):
         total_sum = sum(nums)
         
         f = sum(i * num for i, num in enumerate(nums))
-        m = f
+        max_val = f
         
         for k in range(1, n):
             f = f + total_sum - n * nums[-k]
-            m = max(m, f)
+            max_val = max(max_val, f)
         
-        return m
+        return max_val
